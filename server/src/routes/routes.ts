a65d0e16bb47c40ecb934701d0e0d70951864265
Nome <miranhaomi@gmail.com>
2024-05-21 15:21:59 -0300

    Criando o controller e rotas dos comentários

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 import { UserController } from '../controller/UserController';
 import { ForumController } from '../controller/DiscussionTopics';
+import { CommentsController } from '../controller/CommentController';
 
 const userController = new UserController();
 const forumController = new ForumController();
-
+const commentsController = new CommentsController();
 
 export const router = Router();
 
@@ -14,3 +15,7 @@ router.get("/users", userController.index)
 // forum routes
 router.post("/foruns", forumController.create);
 router.get("/foruns", forumController.list);
+
+// comments routes
+router.post("/comments", commentsController.create);
+router.get("/comments/:discussionId", commentsController.list);
