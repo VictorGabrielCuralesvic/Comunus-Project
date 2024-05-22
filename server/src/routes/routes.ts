2ffeea74d165484aa76ed0a46d4b97e54c043beb
Nome <miranhaomi@gmail.com>
2024-05-22 10:26:42 -0300

    Controller dos seguidores e rotas dos seguidores
    Controller voltado para a possibilidade de um usuário seguir, deixar de seguir, ver a lista de seguidores e não seguidores do outro usuário. Além disso, as rotas https.

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -2,10 +2,12 @@ import {Router} from 'express';
 import { UserController } from '../controller/UserController';
 import { ForumController } from '../controller/DiscussionTopics';
 import { CommentsController } from '../controller/CommentController';
+import { FollowerController } from '../controller/FollowerController';
 
 const userController = new UserController();
 const forumController = new ForumController();
 const commentsController = new CommentsController();
+const followerController = new FollowerController();
 
 export const router = Router();
 
@@ -19,3 +21,9 @@ router.get("/foruns", forumController.list);
 // comments routes
 router.post("/comments", commentsController.create);
 router.get("/comments/:discussionId", commentsController.list);
+
+// follower routes
+router.post("/follow", followerController.follow);
+router.post("/unfollow", followerController.unfollow);
+router.get("/followers/:userId", followerController.listFollowers);
+router.get("/following/:userId", followerController.listFollowing);
