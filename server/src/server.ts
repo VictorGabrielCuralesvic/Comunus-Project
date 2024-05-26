579ca59a20b767c16a0bfa08d70fcddb4be1fb95
Ronald <ronaldmacario13@gmail.com>
2024-05-26 18:39:34 -0300

    chore: Add cors package for handling cross-origin resource sharing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { router } from "./routes/routes";
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
