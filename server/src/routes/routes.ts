653389a81cf517a3509ff4325da3526e8fb41284
Ronald <ronaldmacario13@gmail.com>
2024-05-26 19:44:40 -0300

    feat: Add authentication middleware for user routes

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,21 +1,26 @@
 import {Router} from 'express';
 import { UserController } from '../controller/UserController';
+import { AuthController } from '../controller/AuthController';
+import { AuthMiddleware } from '../middlewares/auth';
 import { ForumController } from '../controller/DiscussionTopics';
 import { CommentsController } from '../controller/CommentController';
 
 const userController = new UserController();
+const authController = new AuthController();
 const forumController = new ForumController();
 const commentsController = new CommentsController();
 
 export const router = Router();
 
-router.post("/createuser", userController.store)
-router.get("/users", userController.index)
+// user routes
+router.post("/createuser", userController.store);
+router.get("/users", AuthMiddleware, userController.index);
+router.post("/auth", authController.authenticate);
 
 // forum routes
-router.post("/foruns", forumController.create);
+router.post("/foruns", AuthMiddleware, forumController.create);
 router.get("/foruns", forumController.list);
 
 // comments routes
-router.post("/comments", commentsController.create);
+router.post("/comments", AuthMiddleware, commentsController.create);
 router.get("/comments/:discussionId", commentsController.list);
